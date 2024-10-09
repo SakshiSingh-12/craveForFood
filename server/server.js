5961563ca6ec8da773432ce91c82da8e68375002
SakshiSingh-12 <139615391+SakshiSingh-12@users.noreply.github.com>
2024-10-09 14:46:01 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,3 +4,9 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+
+
+const app = express();
+const PORT = 5000;
+
+
