7aa2a38a5c67ee7397fcaf9fc337fc7b50594af5
SakshiSingh-12 <139615391+SakshiSingh-12@users.noreply.github.com>
2024-10-09 14:47:55 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -47,3 +47,8 @@ app.get('/api/restaurants/:id/food_items', (req, res) => {
     });
 });
 
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
