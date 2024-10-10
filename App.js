36a4af0472ba57409a92319ae65debb3529c436f
SakshiSingh-12 <139615391+SakshiSingh-12@users.noreply.github.com>
2024-10-10 11:41:03 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,1 @@
 // src/App.js
-
-import React from 'react';
-import Home from './Home';
-
-const App = () => {
-    return (
-        <div>
-            <Home />
-        </div>
-    );
-};
-
-export default App;
