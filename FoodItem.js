414f9645f61f1e096e4722d87b047b51a4097e8c
SakshiSingh-12 <139615391+SakshiSingh-12@users.noreply.github.com>
2024-10-11 11:37:28 +0530

    Update FoodItem.js

--- FoodItem.js
+++ FoodItem.js
@@ -3,3 +3,9 @@
 
 import React, { useState } from 'react';
 
+const FoodItem = ({ food, onAdd }) => {
+    const [quantity, setQuantity] = useState(1);
+
+    const handleIncrease = () => {
+        setQuantity((prevQuantity) => prevQuantity + 1);
+    };
