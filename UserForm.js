75a2f6ee6b93d83246fa889f62051472e113f39b
SakshiSingh-12 <139615391+SakshiSingh-12@users.noreply.github.com>
2024-10-12 13:09:54 +0530

    Update UserForm.js

--- UserForm.js
+++ UserForm.js
@@ -3,6 +3,12 @@ import React from 'react';
 
 const UserForm = () => {
     // Simple User Form (could be expanded)
+     return (
+        <form>
+            <input type="text" placeholder="Enter your name" style={styles.input} />
+            <input type="text" placeholder="Enter your address" style={styles.input} />
+        </form>
+    );
    
 };
 
