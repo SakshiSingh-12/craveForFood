99010e76fd1a8f772b883b22dc5b852a43ea284e
SakshiSingh-12 <139615391+SakshiSingh-12@users.noreply.github.com>
2024-10-13 11:01:13 +0530

    Update SearchBar.js

--- SearchBar.js
+++ SearchBar.js
@@ -2,11 +2,7 @@
 import React, { useState } from 'react';
 
 const SearchBar = ({ onSearch }) => {
-    const [query, setQuery] = useState('');
-
-    const handleInputChange = (e) => {
-        setQuery(e.target.value);
-        onSearch(e.target.value); // Trigger search
+   
     };
 
     return (
