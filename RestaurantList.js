9d99a6d0c0af0e425ceccc5e7a9113154b50d47a
SakshiSingh-12 <139615391+SakshiSingh-12@users.noreply.github.com>
2024-10-14 11:09:22 +0530

    Update RestaurantList.js

--- RestaurantList.js
+++ RestaurantList.js
@@ -4,5 +4,15 @@
 import React, { useState } from 'react';
 import RestaurantDetail from './RestaurantDetail';
 
+const RestaurantList = ({ restaurants, onAddItem }) => {
+    const [selectedRestaurant, setSelectedRestaurant] = useState(null);
 
+    const handleRestaurantClick = (restaurant) => {
+        setSelectedRestaurant(restaurant);
+    };
 
+    const handleCloseDetail = () => {
+        setSelectedRestaurant(null);
+    };
+
+   
