2f943422807d057d8a91c5d41991e8a58f545684
SakshiSingh-12 <139615391+SakshiSingh-12@users.noreply.github.com>
2024-10-16 11:18:24 +0530

    Update PaymentForm.js

--- PaymentForm.js
+++ PaymentForm.js
@@ -1,6 +1,5 @@
 // src/components/PaymentForm.js
 import React from 'react';
-
 const PaymentForm = ({ amount }) => {
     const handlePayment = () => {
         alert(`Processing payment of $${amount}`);
